1313a1f5a080083eacb00105fe9ac31b74c6b091
wisekim <lwisekiml@naver.com>
2024-01-31 00:10:24 +0900

    08. 비지니스 로직 구현 - JobParametersValidator와 Incrementer

--- src/main/java/me/wisekim/fcp/job/expire/ExpirePointJobConfiguration.java
+++ src/main/java/me/wisekim/fcp/job/expire/ExpirePointJobConfiguration.java
@@ -1,8 +1,10 @@
 package me.wisekim.fcp.job.expire;
 
+import me.wisekim.fcp.job.validator.TodayJobParameterValidator;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,10 +13,13 @@ public class ExpirePointJobConfiguration {
     @Bean
     public Job expirePointJob(
             JobBuilderFactory jobBuilderFactory,
+            TodayJobParameterValidator validator,
             Step expirePointStep
 
     ) {
         return jobBuilderFactory.get("expirePointJob")
+                .validator(validator)
+                .incrementer(new RunIdIncrementer()) // run.id가 계속 해서 증가해서 job parameter가 중복되지 않게 해줌(즉 Job 중복 실행 가능)
                 .start(expirePointStep)
                 .build();
     }
